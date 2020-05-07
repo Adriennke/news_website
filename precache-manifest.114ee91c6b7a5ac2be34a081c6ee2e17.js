12bc486e34ce7fe93ce5ae21c8e5fb15b05e820a
Adrienn <adrienntollas@gmail.com>
2020-05-07 14:33:07 +0200

    Updates

---
+++ precache-manifest.114ee91c6b7a5ac2be34a081c6ee2e17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e2618223dc2d94e5e709de62189759",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f2446f4de4433836e1a",
+    "url": "/static/css/main.36bcfd17.chunk.css"
+  },
+  {
+    "revision": "1914af9684ae1f20a78b",
+    "url": "/static/js/2.280ab8c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.280ab8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2446f4de4433836e1a",
+    "url": "/static/js/main.579a07bd.chunk.js"
+  },
+  {
+    "revision": "82acfd11bbd371f15f6c",
+    "url": "/static/js/runtime-main.e29bf74e.js"
+  },
+  {
+    "revision": "9761b8004adf129b160a0a6c0c5fa793",
+    "url": "/static/media/news.9761b800.jpg"
+  }
+]);
